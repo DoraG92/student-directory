c713bc40b4244fa9268f3a092da7b794bd8c84b5
Dora Gores <d.lopesgores@gmail.com>
2023-01-09 15:53:25 +0000

    Add comments

--- directory.rb
+++ directory.rb
@@ -11,7 +11,8 @@ puts "freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-
+# Finally, we print the total number of students
 print "Overall, we have "
+# It's important that print() doesn't add new line characters
 print 11
 print " great students"
