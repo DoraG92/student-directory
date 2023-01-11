6d6d72b8290cd731856e60fb9e1d693c6986eed3
Dora Gores <d.lopesgores@gmail.com>
2023-01-11 15:58:50 +0000

    Added a method to save the students into a csv file

--- directory.rb
+++ directory.rb
@@ -1,4 +1,20 @@
 @students = []
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #create an empty array
+  # get the first name
+  name = gets.chomp
+  while !name.empty? do
+    #add the student hash to the array
+    @students << {name: name, cohort: :november} 
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+end
+
 def interactive_menu
   loop do
     print_menu
@@ -9,9 +25,15 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9.Exit"
 end
 
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
 
 def process(selection)
   case selection
@@ -21,17 +43,13 @@ def process(selection)
     print_header
     print(students)
     print_footer(students)
+  when "3"
+    save_students
   when "9"
   exit 
   else 
   puts "I don't know what you meant, try again"
   end
-end
-
-def show_students
-  print_header
-  print_students_list
-  print_footer
 end
 
 def print_header
@@ -49,19 +67,14 @@ def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  #create an empty array
-  # get the first name
-  name = gets.chomp
-  while !name.empty? do
-    #add the student hash to the array
-    @students << {name: name, cohort: :november} 
-    puts "Now we have #{@students.count} students"
-    # get another name from the user
-    name = gets.chomp
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
   end
+  file.close
 end
 
 interactive_menu
